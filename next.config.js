93fd4426182b2924cc288e4fa08aa89d15da60bf
Hasan Mehmood <hmehmood121@gmail.com>
2022-11-02 00:11:48 +0500

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://links.papareact.com/bdb'],
+  }, 
+  env: {
+    NEXT_PUBLIC_MARKETPLACE_CONTRACT: "0xd9797847457E66Dd1f8F9f37Df8b75dA6eA6A6e4"
+  }
 }
